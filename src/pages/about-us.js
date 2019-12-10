fbe59500fc27eb4f8651937ed7aeded8dfb7a534
Yogesh Jadhav <yogesh18.ivision@gmail.com>
2019-12-10 17:04:52 +0530

    Update about us page

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -1,14 +1,36 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { graphql } from 'gatsby'
 class AboutUs extends React.Component {
     render(){
+        const data = this.props.data.prismicAboutus.data;
         return(
             <Layout>
-                <h3>
-                    Hello, This Is About Us Page...
-                </h3>
+                <h3> {data.sub_title.text}</h3>
+                <h5>{data.title.text}</h5>
+                <p> {data.description.text}</p>
             </Layout>
         )
     }
 }
 export default AboutUs;
+
+export const AboutPage = graphql`{
+    prismicAboutus {
+      data {
+        sub_title{
+            text
+        }
+        title {
+            text
+        }
+        description {
+            text
+        }
+        image {
+            url
+        }
+      }
+    }
+}
+`
