5a66496ed50c63a92027056e1b225edfba7dc23e
Yogesh Jadhav <yogesh18.ivision@gmail.com>
2019-12-24 19:52:05 +0530

    Update Home Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,65 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import img2 from '../styles/images/banner.jpg'
 import { Link } from "gatsby"
+import Commerical from "./our-verticals/commercial"
+import Residential from "./our-verticals/residential"
+import Hospitality from "./our-verticals/hospitality"
+import Leisure from "./our-verticals/leisure"
 
-class IndexPage extends React.Component{
+class IndexPage extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      selectedVertical: null,
+      residential: null,
+      commercial: null,
+      hospitality: null,
+      leisure: null
+    }
+  }
 
-  residencial(){
-    alert('residencial')
+  componentWillMount() {
+    const residentialData = this.props.data.prismicResidential;
+    this.setState({residential: residentialData})
+    this.setState({selectedVertical : < Residential gallery = {residentialData}/>})
+
+    const commercialData = this.props.data.prismicCommercial.data;
+    this.setState({commercial: commercialData})
+    this.setState({selectedVertical : < Commerical gallery = { commercialData } />})
+
+    const hospitalityData = this.props.data.prismicHospitality.data;
+    this.setState({hospitality: hospitalityData})
+    this.setState({selectedVertical : < Hospitality gallery = { hospitalityData } />})
+
+    const leisureData  = this.props.data.prismicLeisureClub.data;
+    this.setState({leisure: leisureData});
+    this.setState({selectedVertical : < Leisure gallery = { leisureData } />})
+  }
+  
+  handleVerticals(event){
+    switch(event){
+      case 'residencial': {
+        this.setState({selectedVertical : < Residential gallery = {this.state.residential}/>})
+      }
+      break;
+      
+      case 'commercial': {
+        this.setState({selectedVertical : < Commerical gallery = { this.state.commercial}/>})
+      }
+      break;
+      
+      case 'hospitality': {
+        this.setState({selectedVertical : < Hospitality gallery = { this.state.hospitality}/>})
+      }
+      break;
+      
+      case 'leisure': {
+        this.setState({selectedVertical : < Leisure gallery = {this.state.leisure} />})
+      }
+      break;
+    }
+    
   }
 
   render(){
@@ -15,33 +67,31 @@ class IndexPage extends React.Component{
     <Layout>
       <SEO title="Home" />
       <div>
-      <section className="banner-section">
-        <img src={img2} alt="banner here" className="banner-img" style={{width:'100%'}}/>
-      </section>
+      {this.state.selectedVertical}
       <section className="our-verticals-tabs container">
         <ul className="p-0 d-flex w-100 justify-content-around list-style-none text-uppercase nav nav-tabs border-0" id="myTab" role="tablist">
           <li className="nav-item active" id="residencial-tab" data-toggle="tab" href="#residencial" role="tab" aria-controls="residencial" aria-selected="true">
             <Link to='/' className="tab">            
                 <i className="far fa-building"></i>
-              <span onClick={this.residencial}> residencial </span>
+              <span onClick={()=> {this.handleVerticals('residencial')}}> residencial </span>
             </Link>
           </li>
           <li className="nav-item"  id="commercial-tab" data-toggle="tab" href="#commercial" role="tab" aria-controls="commercial" aria-selected="true">
             <a href="#" className="tab">            
               <i className="fas fa-hotel"></i>
-              <span>commercial</span>
+              <span onClick={()=> {this.handleVerticals('commercial')}}>commercial</span>
             </a>
           </li>
           <li className="nav-item"  id="hospitality-tab" data-toggle="tab" href="#hospitality" role="tab" aria-controls="hospitality" aria-selected="true">
             <a href="#" className="tab">            
                 <i className="far fa-hospital"></i>
-              <span>hospitality</span>
+              <span onClick={()=> {this.handleVerticals('hospitality')}}>hospitality</span>
             </a>
           </li>
           <li className="nav-item"  id="leisure-tab" data-toggle="tab" href="#leisure" role="tab" aria-controls="leisure" aria-selected="true">
             <a href="#" className="tab">            
                 <i className="fas fa-hot-tub"></i>
-              <span>leisure</span>
+              <span onClick={()=> {this.handleVerticals('leisure')}}>leisure</span>
             </a>
           </li>
         </ul>
@@ -52,6 +102,45 @@ class IndexPage extends React.Component{
   }
 }
 
-
-
 export default IndexPage
+
+
+export const pageDataResidential = graphql`{
+  prismicResidential{
+    data{
+			gallery{
+        image{
+          url
+        }
+      }
+    }
+  }
+  prismicCommercial{
+    data{
+      gallery{
+        image{
+          url
+        }
+      }
+    }
+  }
+  prismicHospitality{
+    data{
+      gallery{
+        image{
+          url
+        }
+      }
+    }
+  }
+  prismicLeisureClub{
+    data{
+      gallery{
+        image{
+          url
+        }
+      }
+    }
+  }
+}`
+
--- src/pages/knoweldge-hub.js
+++ src/pages/knoweldge-hub.js
@@ -3,6 +3,7 @@ import Layout from '../components/layout'
 import hero1 from '../styles/images/hero1.png';
 import aboutLoan from '../styles/images/about-loan.jpg';
 import guidelines1 from '../styles/images/guidelines1.jpg';
+import mahareraa from '../styles/images/mahareha.png'
 import { Link } from '@reach/router';
 import Footer from '../components/footer';
 import { graphql } from 'gatsby';
@@ -10,50 +11,48 @@ class KnoweldgeHub extends React.Component {
   render(){
     const postData = this.props.data.allPrismicKnowledgeHubPost.edges;
     const data = this.props.data.prismicKnoweldeHubMain.data;
-    console.log('data',data);
-    
     console.log('data from knowledge hub page', postData);
     return(
       <Layout>
-        <section className="knowledge-hub-section container container-sm-fluid bg-color">
+        <section className="page-heading-section container container-sm-fluid bg-color">
           <div className="padding-block-60">
-            <h2 className="page-heading">{data.title.text}</h2> 
+              <h2 className="page-heading">{data.title.text}</h2> 
           </div>
-          <div className="row mr-0">
-            <div className="col-12">
-              <div className=" m-0" dangerouslySetInnerHTML={{__html: data.description.html}}/>
+         <div className="row mr-0">
+          <div className="col-12">
+            <div className="main-paragraph">
+              <p className=" m-0" dangerouslySetInnerHTML={{__html: data.description.html}} />
             </div>
           </div>
+        </div>
+        
         </section>
         <section className="knowledge-hub mt-0 mt-md-5">
-          {
-          console.log('item.length-1', postData[postData.length - 1]),
-          //  postData[postData.length - 1],
+          {/* {
+          postData[postData.length - 1],
           <div className=" buyer-tips-banner">
             <div className="container container-sm-fluid">
               <div className="heading">
-                <h3 className="text-center text-uppercase section-title">{postData[postData.length - 1].node.data.title.text}</h3>
+                  <h3 className="text-center text-uppercase section-title">{postData[postData.length - 1].node.data.title.text}</h3>
               </div>
-              <div className="position-relative buyer-tips-banner-img">
-                <div className="buyer-tips-image">
-                  <img  class="lazyload" src={postData[postData.length - 1].node.data.banner.url } alt="" width="100%"/>
+                <div className="position-relative buyer-tips-banner-img">
+                  <div className="buyer-tips-image">
+                    <img src={postData[postData.length - 1].node.data.banner.url } alt="" width="100%"/>
+                  </div>
+                  <div className="buyer-tips-box position-absolute  d-flex justify-content-between flex-column">
+                    <h3 className=" text-uppercase m-0 section-title">{postData[postData.length - 1].node.data.heading.text}</h3>
+                    <p className="mb-0 text-white">{postData[postData.length - 1].node.data.short_description.text}</p>
+                      <div className="know-more">
+                        <a href="#">
+                            <span className="mr-3">Read More</span>
+                            <i className="fas fa-arrow-right"></i>
+                        </a>
+                      </div>
+                  </div>
                 </div>
-                <div className="buyer-tips-box position-absolute  d-flex justify-content-between flex-column">
-                  <h3 className=" text-uppercase m-0 section-title">{postData[postData.length - 1].node.data.title.text}</h3>
-                  <p className="mb-0 text-white">Learn all about tax planning and documentation by reading all the FAQs and more…</p>
-                    <div className="know-more">
-                      <a href="#">
-                        <span className="mr-3">Read More</span>
-                        <i className="fas fa-arrow-right"></i>
-                      </a>
-                    </div>
-                </div>
-              </div>
             </div>
           </div>
           }
-
-
           <div className="buyer-tips-info">
             <div className="container">
               <div className="row">
@@ -65,7 +64,7 @@ class KnoweldgeHub extends React.Component {
                     <div className="row mt-25">
                       <div className="col-md-12 col-lg-6 col-6 pr-0 pr-md-3 pr-lg-0">
                         <div className="card-primary-img">
-                          <img src={aboutLoan} alt="" width="100%"/>
+                          <img src={hero1} alt="" width="100%"/>
                         </div>
                       </div>
                       <div className="col-md-12 col-lg-6  col-6 pl-0 pl-md-3 pl-lg-0 ">
@@ -83,10 +82,10 @@ class KnoweldgeHub extends React.Component {
                   </div>
                 </div>
                 <div className="col-md-6 p-0 pl-md-3 pr-md-3 ">
-                  <div className="guidelines-nris mt-100">
-                    <div className="heading">
-                      <h3 className=" text-uppercase mb-3 section-title">Guidelines for NRIs</h3> 
-                    </div>
+                    <div className="guidelines-nris mt-100">
+                      <div className="heading">
+                        <h3 className=" text-uppercase mb-3 section-title">Guidelines for NRIs</h3> 
+                      </div>
                     <div className="guidelines-nris-wrapper position-relative mt-25">
                       <div className=" guidelines-nris-img">  
                         <img src={guidelines1} alt="" width="100%"/>
@@ -94,34 +93,8 @@ class KnoweldgeHub extends React.Component {
                       <div className="guidelines-nris-info position-absolute">
                         <p className="mb-0 mb-sm-2 text-white">Guidelines For NRIs</p>
                         <div className="know-more">
-                          <Link to='/nri'>
-                            <span className="mr-sm-5 mr-3">Know More</span>
-                            <i className="fas fa-arrow-right"></i>
-                          </Link>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            <div className="row">
-              <div className="col-md-6 ">
-                <div className="maha-rera card-primary mt-70">
-                  <div className="heading ">
-                    <h3 className=" text-uppercase mb-3 section-title">Maha Rera</h3>
-                  </div>
-                  <div className="row mt-25">
-                    <div className="col-md-12 col-lg-6 col-6 pr-0 pr-md-3 pr-lg-0">
-                      <div className="card-primary-img">
-                        <img src={aboutLoan} alt="" width="100%"/>
-                      </div>
-                    </div>
-                    <div className="col-md-12 col-lg-6 col-6 pl-0 pl-md-3 pl-lg-0">
-                      <div className=" p-sm-4 d-flex justify-content-between flex-column bg-light-gray">
-                        <p>Praesent gravida augue eu metus interdum, ac elementum metus blandit.</p>
-                        <div className="know-more">
                           <a href="#">
-                            <span className="mr-3">Know More</span>
+                            <span className="mr-sm-5 mr-3">Know More</span>
                             <i className="fas fa-arrow-right"></i>
                           </a>
                         </div>
@@ -130,10 +103,11 @@ class KnoweldgeHub extends React.Component {
                   </div>
                 </div>
               </div>
+              <div className="row">
                 <div className="col-md-6 ">
-                  <div className="important-notices card-primary mt-70">
-                    <div className="heading">
-                      <h3 className=" text-uppercase mb-3 section-title">Important Notices</h3>
+                  <div className="maha-rera card-primary mt-70">
+                    <div className="heading ">
+                      <h3 className=" text-uppercase mb-3 section-title">Maha Rera</h3>
                     </div>
                     <div className="row mt-25">
                       <div className="col-md-12 col-lg-6 col-6 pr-0 pr-md-3 pr-lg-0">
@@ -143,12 +117,37 @@ class KnoweldgeHub extends React.Component {
                       </div>
                       <div className="col-md-12 col-lg-6 col-6 pl-0 pl-md-3 pl-lg-0">
                         <div className=" p-sm-4 d-flex justify-content-between flex-column bg-light-gray">
+                          <p>Praesent gravida augue eu metus interdum, ac elementum metus blandit.</p>
+                          <div className="know-more">
+                            <a href="#">
+                              <span className="mr-3">Know More</span>
+                              <i className="fas fa-arrow-right"></i>
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-6 ">
+                  <div className="important-notices card-primary mt-70">
+                    <div className="heading">
+                      <h3 className=" text-uppercase mb-3 section-title">Important Notices</h3>
+                    </div>
+                    <div className="row mt-25">
+                      <div className="col-md-12 col-lg-6 col-6 pr-0 pr-md-3 pr-lg-0">
+                        <div className="card-primary-img">
+                          <img src={hero1} alt="" width="100%"/>
+                        </div>
+                      </div>
+                      <div className="col-md-12 col-lg-6 col-6 pl-0 pl-md-3 pl-lg-0">
+                        <div className=" p-sm-4 d-flex justify-content-between flex-column bg-light-gray">
                           <h3 className="inner-section-title">Important Notices</h3>
                           <p>Praesent gravida augue eu metus interdum, ac elementum metus blandit.</p>
                           <div className="know-more">
                             <a href="#">
-                            <span className="mr-3">Know More</span>
-                            <i className="fas fa-arrow-right"></i>
+                              <span className="mr-3">Know More</span>
+                              <i className="fas fa-arrow-right"></i>
                             </a>
                           </div>
                         </div>
@@ -158,7 +157,149 @@ class KnoweldgeHub extends React.Component {
                 </div>
               </div>
             </div>
+          </div> */}
+          <div className=" buyer-tips-banner">
+      <div className="container container-sm-fluid">
+        <div className="heading">
+            <h3 className="text-center text-uppercase section-title">Buyer’s Tips</h3>
+        </div>
+          <div className="position-relative buyer-tips-banner-img">
+            <div className="buyer-tips-image">
+              <img src={hero1} alt="" width="100%"/>
+            </div>
+            <div className="buyer-tips-box position-absolute  d-flex justify-content-between flex-column">
+              <h3 className=" text-uppercase m-0 section-title">Buyer’s tips</h3>
+              <p className="mb-0 text-white">Learn all about tax planning and documentation by reading all the FAQs and more…</p>
+                <div className="know-more">
+                  <a href="#">
+                      <span className="mr-3">Read More</span>
+                      <i className="fas fa-arrow-right"></i>
+                  </a>
+                </div>
+            </div>
           </div>
+      </div>
+    </div>
+    <div className="buyer-tips-info">
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6">
+                    <div className="about-loan card-primary mt-140">
+                        <div className="heading">
+                            <h3 className=" text-uppercase mb-2 mb-sm-3 section-title ">About Loan</h3>
+                        </div>
+                        <div className="row mt-25">
+                            <div className="col-md-12 col-lg-6 col-6 pr-0 pr-md-3 pr-lg-0">
+                                <div className="card-primary-img">
+                                     <img src={aboutLoan} alt="" width="100%"/>
+                                </div>
+                            </div>
+                            <div className="col-md-12 col-lg-6  col-6 pl-0 pl-md-3 pl-lg-0 ">
+                                <div className="  d-flex justify-content-between flex-column bg-light-gray">
+                                    <p>Praesent gravida augue eu metus interdum, ac elementum metus blandit.</p>
+                                    <div className="know-more">
+                                        <a href="#">
+                                            <span className="mr-3">Know More</span>
+                                            <i className="fas fa-arrow-right"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6 p-0 pl-sm-3 pr-sm-3 ">
+                    <div className="guidelines-nris mt-140">
+                      <div className="heading">
+                        <h3 className=" text-uppercase mb-3 section-title">Guidelines for NRIs</h3> 
+                      </div>
+                    <div className="guidelines-nris-wrapper position-relative mt-25">
+                        <div className=" guidelines-nris-img">  
+                          <img src={guidelines1} alt="" width="100%"/>
+                        </div> 
+                        <div className="guidelines-nris-info position-absolute">
+                          <p className="mb-0 mb-sm-2 text-white">Guidelines For NRIs</p>
+                          <div className="know-more">
+                            <a href="#">
+                                <span className="mr-sm-5 mr-3">Know More</span>
+                                <i className="fas fa-arrow-right"></i>
+                            </a>
+                          </div>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-6 d-none d-sm-block">
+                    <div className="maha-rera card-primary mt-92 ">
+                        <div className="heading ">
+                            <h3 className=" text-uppercase mb-3 section-title">Maha Rera</h3>
+                        </div>
+                        <div className="row mt-25">
+                            <div className="col-md-12 col-lg-6 col-6 pr-0 pr-md-3 pr-lg-0">
+                                <div className="card-primary-img">
+                                      <img src={aboutLoan} alt="" width="100%"/>
+                                </div>
+                            </div>
+                            <div className="col-md-12 col-lg-6 col-6 pl-0 pl-md-3 pl-lg-0">
+                                <div className="  d-flex justify-content-between flex-column bg-light-gray">
+                                    <p>Praesent gravida augue eu metus interdum, ac elementum metus blandit.</p>
+                                    <div className="know-more">
+                                        <a href="#">
+                                            <span className="mr-3">Know More</span>
+                                            <i className="fas fa-arrow-right"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6 d-sm-none p-0">
+                  <div className="maha-rera-mb d-flex position-relative">
+                    <img src={mahareraa} alt="" className="pl-4"/>
+                    <div className="ml-3 ">
+                      <h3 className="inner-section-title mb-1">Maha <span className="text-uppercase">Rera</span> </h3>
+                      <p className="m-0">Rules and regulations</p>
+                    </div>
+                    <div className="know-more d-flex align-items-end justify-content-end position-absolute">
+                      <a href="#">
+                          <span  className="mr-3">Know More</span>
+                          <i className="fas fa-arrow-right"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                    <div className="col-md-6 ">
+                        <div className="important-notices card-primary mt-92">
+                            <div className="heading">
+                                <h3 className=" text-uppercase mb-3 section-title">Important Notices</h3>
+                            </div>
+                              <div className="row mt-25">
+                                  <div className="col-md-12 col-lg-6 col-6 pr-0 pr-md-3 pr-lg-0">
+                                      <div className="card-primary-img">
+                                            <img src={aboutLoan} alt="" width="100%"/>
+                                      </div>
+                                  </div>
+                                  <div className="col-md-12 col-lg-6 col-6 pl-0 pl-md-3 pl-lg-0">
+                                      <div className="  d-flex justify-content-between flex-column bg-light-gray">
+                                        <h3 className="inner-section-title">Important Notices</h3>
+                                          <p>Praesent gravida augue eu metus interdum, ac elementum metus blandit.</p>
+                                          <div className="know-more">
+                                              <a href="#">
+                                                  <span className="mr-3">Know More</span>
+                                                  <i className="fas fa-arrow-right"></i>
+                                              </a>
+                                          </div>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                    </div>
+                </div>
+        </div>
+    </div>
         </section>
         <Footer />
       </Layout>
@@ -180,6 +321,9 @@ export const knowledgePage = graphql`{
         }
         description{
           html
+        }
+        short_description{
+          text
         }
         heading{
           text
