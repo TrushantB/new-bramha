cece1599d6e7d1869d971516ef71ffbd67232d58
bepardeshi <bpareshi.ivision@gmail.com>
2020-01-08 17:54:10 +0530

    pull the code be

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,4 +3,5 @@ import 'jquery/dist/jquery.min.js'
 import 'popper.js/dist/popper.min'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import 'lazysizes'
+
 // import './node_modules/lazysizes/lazysizes';
