543fa71ed5f833f1cea89f613c5fb6581e84b2bc
bepardeshi <bpareshi.ivision@gmail.com>
2020-01-09 14:29:00 +0530

    About us slider updated

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -23,7 +23,8 @@ class AboutUs extends React.Component {
       speed: 1000,
       slidesToShow: 1,
       slidesToScroll: 1,
-      autoplay : true,
+      // autoplay : true,
+      arrows:false,
       customPaging: i => (
         <div>
           <div key={i}> {data.our_legacy.document[0].data.our_legacy[i].year}</div>
@@ -94,7 +95,7 @@ class AboutUs extends React.Component {
               </div>
               <div className="slider-wrapper">
                 <div className="slider-border"></div>
-                <div className="container">
+                <div className="container-fluid container-md slider-container">
                 <Slider {...settings}>
                     {
                       data.our_legacy.document[0].data.our_legacy.map((item,value)=>{
