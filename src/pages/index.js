56a95e244c1393e51634bf3574d946c406016990
bepardeshi <bpareshi.ivision@gmail.com>
2020-01-17 19:01:24 +0530

    changes updated be

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Commerical from '../pages/our-verticals-main/commercial';
 import Residential from "../pages/our-verticals-main/residential";
 import Hospitality from "../pages/our-verticals-main/hospitality";
 import Leisure from "../pages/our-verticals-main/leisure";
-
+import $ from 'jquery';
 class IndexPage extends React.Component {
   constructor() {
     super();
@@ -16,6 +16,9 @@ class IndexPage extends React.Component {
       commercial: null,
       hospitality: null,
       leisure: null,
+      contactFlag:false,
+      mailFlag:false,
+      chatFlag:false,
       ourVerticals:[
         {
           id:'residential',
@@ -43,6 +46,8 @@ class IndexPage extends React.Component {
   }
 
   componentWillMount() {
+    // $("#navdrawer").navdrawer();
+
     const residentialData = this.props.data.prismicResidential;
     this.setState({residential: residentialData})
     this.setState({selectedVertical : < Residential gallery = {residentialData}/>})
@@ -87,13 +92,86 @@ class IndexPage extends React.Component {
       else item.class="";
     })
   }
-
   render() {
+    // let contactFlag=false;
+    // function classHandle() {
+    //   $("body").removeClass("drewer-open");
+    //  }
     return (
       <Layout>
         <SEO title="Home"/>
         <div>
-          { this.state.selectedVertical}
+          { this.state.selectedVertical}      
+          <div className="drawer-container">
+            <div className="drawer-toggle" id="toggle" onClick={() => this.setState({contactFlag:!this.state.contactFlag})}>
+              <i className="fas fa-phone"></i>
+            </div>
+            <div className="drawer-toggle" onClick={() => this.setState({mailFlag:!this.state.mailFlag})}>
+              <i className="fas fa-envelope-open-text"></i>
+            </div>
+            <div className="drawer-toggle" onClick={() => this.setState({chatFlag:!this.state.chatFlag})}>
+              <i className="fas fa-comment-alt"></i>
+            </div>
+          </div>
+          {
+              this.state.contactFlag && 
+            <div className="drawer-field" id="navigation" >
+              <div className="hide text-right" onClick={() => this.setState({contactFlag:false})}>
+                <i className="fas fa-times"></i>
+              </div>
+              Hi this is contact
+            </div>
+
+            }
+            {
+              this.state.mailFlag && 
+            <div className="drawer-field" id="navigation" >
+              <div className="hide text-right" onClick={() => this.setState({mailFlag:false})}>
+                <i className="fas fa-times"></i>
+              </div>
+              <div className="mail">
+                <form name="customer" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+                  <div className="contact-form-bg" id="customer">
+                      <div className="container">
+                        <div className="form-row">
+                          <div className="col-sm-12 form-group  ">
+                              <input type="text" className="form-control" id="name" placeholder="Your Name*" name="name" autoComplete="false" required/>
+                          </div>
+                          <div className="col-sm-12 form-group  ">
+                              <input type="text" className="form-control" id="email" placeholder="Your Email*" autoComplete="false" name="email" required/>
+                          </div>
+                          <div className="col-sm-12 form-group  ">
+                              <input type="number" className="form-control" id="phone-number" placeholder="Your Phone Number*" name="phone-number" required/>
+                          </div>
+                          <div className="col-sm-12 form-group  ">
+                              <input type="number" className="form-control" id="budget" placeholder="Budget" name="budget" required/>
+                          </div>
+                          <div className="col-sm-12 form-group  ">
+                              <input type="text" className="form-control" id="city" placeholder="City" name="city" required/>
+                          </div>
+                          <div className="form-group col-md-12">
+                            <textarea className="form-control" rows="5" id="message" placeholder="Additional Message" name="message" required></textarea>
+                          </div>
+                        </div>
+                        <div className="sumbit text-center mt-sm-0 mt-4">
+                          <button type="submit" className="btn-secondary">Submit</button>
+                        </div>  
+                      </div> 
+                    </div>
+                </form>
+              </div>
+            </div>
+            }
+            {
+              this.state.chatFlag && 
+            <div className="drawer-field" id="navigation" >
+              <div className="hide text-right" onClick={() => this.setState({chatFlag:false})}>
+                <i className="fas fa-times"></i>
+              </div>
+              Plugin for chat goes here...!
+            </div>
+
+            }
           <footer className="our-verticals-tabs d-flex">
             <div className="container d-flex">
               <ul className="p-0 d-flex w-100 justify-content-around list-style-none text-uppercase nav nav-tabs border-0" id="myTab" role="tablist">
