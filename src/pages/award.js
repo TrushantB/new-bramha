b78e0aaf6e7010eca668250829bd7f843cc1275c
Yogesh Jadhav <yogesh18.ivision@gmail.com>
2020-01-17 17:42:14 +0530

    Update award page

--- src/pages/award.js
+++ src/pages/award.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import Layout from '../components/layout';
 import Footer from '../components/footer';
+import SEO from '../components/seo';
+import { graphql } from 'gatsby';
 class Award extends React.Component {
     render(){
+        const awardData = this.props.data.prismicAwardAndRecognition.data;
         return(
             <Layout>
+                <SEO title={awardData.title.text}/>
                 <section className="award-wrapper">
                     <section className="page-heading-section container container-sm-fluid bg-color">
                         <div className="padding-block-60">
-                            <h2 className="page-heading">Awards & Recognition</h2> 
+                            <h2 className="page-heading">{awardData.title.text}</h2> 
                         </div>
                         <div className="row mr-0">
                             <div className="col-12">
                             <p className="main-paragraph mb-0">
-                                BramhaCorp welcomes you to get in touch with us. It has been our endeavor to exceed our customer's expectations at all times. Please communicate with us by telling us what you are looking for.
+                                {awardData.short_description.text}
                             </p>   
                             </div>
                         </div>
@@ -22,60 +26,29 @@ class Award extends React.Component {
                     <section className="award-name">
                         <div className="container">
                             <div className="row">
-                                <div className="col-lg-4 col-sm-6 mb-120">
-                                   <div className="award-bg">
-                                      <div className="award-details">
-                                        <p className="inner-section-title text-center">
-                                            Ranked 12th amongst the top 15 real estate developers by crisis
-                                        </p>
-                                      </div>
-                                   </div>
-                                   <div className="company-name">
-                                       <h5 className="inner-section-title text-center text-uppercase">
-                                           BramhaCorp Ltd
-                                       </h5>
-                                       <p className="award-by text-center text-uppercase">
-                                           By Crisil
-                                       </p>
-                                   </div>
-                                </div>
-
-                                <div className="col-lg-4 col-sm-6 mb-120">
-                                   <div className="award-bg">
-                                      <div className="award-details">
-                                        <p className="inner-section-title text-center">
-                                            Ranked 12th amongst the top 15 real estate developers by crisis
-                                        </p>
-                                      </div>
-                                   </div>
-                                   <div className="company-name">
-                                       <h5 className="inner-section-title text-center text-uppercase">
-                                           BramhaCorp Ltd
-                                       </h5>
-                                       <p className="award-by text-center text-uppercase">
-                                           By Crisil
-                                       </p>
-                                   </div>
-                                </div>
-
-                                <div className="col-lg-4 col-sm-6 mb-120">
-                                   <div className="award-bg">
-                                      <div className="award-details">
-                                        <p className="inner-section-title text-center">
-                                            Ranked 12th amongst the top 15 real estate developers by crisis
-                                        </p>
-                                      </div>
-                                   </div>
-                                   <div className="company-name">
-                                       <h5 className="inner-section-title text-center text-uppercase">
-                                           BramhaCorp Ltd
-                                       </h5>
-                                       <p className="award-by text-center text-uppercase">
-                                           By Crisil
-                                       </p>
-                                   </div>
-                                </div>
-
+                                {
+                                    awardData.awards.map((item, index) => {
+                                        return(
+                                            <div key={index} className="col-lg-4 col-sm-6 mb-120">
+                                            <div className="award-bg">
+                                                <div className="award-details">
+                                                    <p className="inner-section-title text-center">
+                                                        {item.description1.text}
+                                                    </p>
+                                                </div>
+                                            </div>
+                                            <div className="company-name">
+                                                <h5 className="inner-section-title text-center text-uppercase">
+                                                    {item.title1.text}
+                                                </h5>
+                                                <p className="award-by text-center text-uppercase">
+                                                   {item.tagline.text}
+                                                </p>
+                                            </div>
+                                            </div>
+                                        )
+                                    })
+                                }
                             </div>
                         </div>
                     </section>
@@ -86,3 +59,30 @@ class Award extends React.Component {
     }
 }
 export default Award;
+
+export const awardPage = graphql`{
+    prismicAwardAndRecognition{
+        data {
+            title {
+                text
+            }
+            short_description {
+                text
+            }
+            awards {
+                title1 {
+                    text
+                }
+                tagline {
+                    text
+                }
+                description1 {
+                    text
+                }
+                image {
+                    url
+                }
+            }
+        }
+    }
+}`
