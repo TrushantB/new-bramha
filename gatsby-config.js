929028ad430f6127d05a839a6dba9761f50c8c0c
Yogesh Jadhav <yogesh18.ivision@gmail.com>
2020-02-12 19:10:54 +0530

    Update residential details page

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,8 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sharp`,
     `gatsby-prismic-lazy-load`,
+    "gatsby-remark-embed-video",
+    "gatsby-remark-responsive-iframe",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -66,6 +68,23 @@ module.exports = {
       options: {
         cachePublic: true
       }
-    }
+    },
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+        {
+          resolve: "gatsby-remark-embed-video",
+          options: {
+            width: 800,
+            ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
+            height: 400, // Optional: Overrides optional.ratio
+            related: false, //Optional: Will remove related videos from the end of an embedded YouTube video.
+            noIframeBorder: true //Optional: Disable insertion of <style> border: 0
+          }
+        }
+        ]
+      }
+    },
   ],
 }
