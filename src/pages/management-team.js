aec201aff5f9770c062f70170ea967a8259236e7
Yogesh Jadhav <yogesh18.ivision@gmail.com>
2020-06-13 16:58:55 +0530

    Resolve issues

--- src/pages/management-team.js
+++ src/pages/management-team.js
@@ -38,7 +38,7 @@ class ManagementTeam extends React.Component {
                       <div key={index} className="col-md-4 col-lg-3 col-sm-6">
                         <div className="member">
                           <div className="founder-image">
-                            <img src={item.image.url} alt="" title="Founder" width="100%"/>
+                            {/* <img src={item.image.url} alt="" title="Founder" width="100%"/> */}
                           </div>
                           <div className="info-content">
                             <h4 className="heading">{item.name.text}</h4>
